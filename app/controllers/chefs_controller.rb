a94e9b5021d6e5842006a5cefea8d0424fc69f1c
Carmen Minikus <carmenminikus@gmail.com>
2016-04-16 18:18:30 +1000

    added create menu button in chefs -display

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,19 @@
 class ChefsController < ApplicationController
   def display
+    @users = User.where(:chef => true)
+    @menus = Menu.all
   end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :password, :email, :chef, :phone, :image, :bio, :rate)
+    end
 end
