937a37b486f43c67b1e6c5a22a1c090228a58823
Sam Humphreys <s-humphreys@hotmail.com>
2016-04-16 13:27:48 +1000

    session controller working

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,6 +5,7 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by :email => params[:email]
+    # raise "hell"
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, :presence => true, :uniqueness => true
+
   has_many :events
   has_many :menus
   has_many :reviews
