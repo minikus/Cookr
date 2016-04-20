2a40c4dfae637a930fcfa8a4b8724021864ed034
Sam Humphreys <s-humphreys@hotmail.com>
2016-04-20 17:15:45 +1000

    gonna start crying soon

--- app/assets/javascripts/messageshow.js
+++ app/assets/javascripts/messageshow.js
@@ -2,9 +2,10 @@ $(document).ready(function () {
   var current_user = 0;
   var users = [];
   var messages = [];
-  var relevantMessages = [];
+  var newMessages = [];
   var conversations = [];
-  var conversationMessages = [];
+  var messageFocus = 0;
+  var pageLoaded = 0;
 
   //get current user id, call this function to kick it off
   var getUserId = function () {
@@ -20,53 +21,68 @@ $(document).ready(function () {
     $.ajax('/get_users').done(function(result) {
       users = result.users;
     }).done(function () {
-      getAllMessages();
+      getMessages();
     });
   };
 
-  // get all messages
-  var getAllMessages = function () {
+  // get all messages where user is target or creator
+  var getMessages = function () {
     $.ajax('/getmessages').done(function (result) {
-      messages = result.messages;
-    }).done(function () {
-      relevantMessages = _.filter(messages, function (message){
-        return (message.user_id === current_user || message.target === current_user);
+      newMessages = _.filter(result.messages, function (message) {
+        return (message.user_id === current_user || message.target === current_user)
       });
-      // puts all of the conversation counterparts in an array
-      _.each(relevantMessages, function (message) {
-        if (message.user_id === current_user) {
-          conversations.push(message.target);
-        } else if (message.target === current_user) {
-          conversations.push(message.user_id);
+      if (messageFocus === 0) {
+        messages = newMessages;
+        getConversations(messages);
+      } else {
+        if (newMessages !== messages) {
+          messages = newMessages;
+          displayMessages(messageFocus, messages);
         };
-      });
-      conversations = _.uniq(conversations).sort();
-
-      // creates "chat heads" for each conversation
-      var width = 95/conversations.length;
-      for (var i = 0; i< conversations.length; i++) {
-        var user = _.find(users, function(user) {
-          return user.id === conversations[i];
-        });
-        $chatHead = $('<div/>').width(width + '%').addClass('chat-head').attr('data', user.id );
-        $chatHead.html('<p>' + user.first_name + '</p>')
-        $chatHead.appendTo($('#conversations'));
-      }
-      //when you click on a chat head it shows the message history with that person
-
-      $('.chat-head').on('click', function () {
-        var person = Number($(this).attr('data'));
-        displayMessages(person, relevantMessages);
-      });
+      };
     });
   };
 
+  //get an array of conversations counterparts
+  var getConversations = function (messages) {
+    _.each(messages, function (message) {
+      if (message.user_id === current_user) {
+        conversations.push(message.target);
+      } else if (message.target === current_user) {
+        conversations.push(message.user_id);
+      };
+    });
+    conversations = _.uniq(conversations).sort();
+    createChatHeads();
+  };
 
+  //creates chat heads for your conversations and appends them to the #conversations div
+  var createChatHeads = function () {
+    var width = 100/conversations.length;
+    for (var i = 0; i< conversations.length; i++) {
+      var user = _.find(users, function (user) {
+        return user.id === conversations[i];
+      });
+      $chatHead = $('<div/>').width(width + '%').addClass('chat-head').attr('data', user.id);
+      $chatHead.html('<p>' + user.first_name + '</p>');
+      $chatHead.appendTo($('#conversations'));
+    };
+    addChatHeadClicker();
+  };
 
-  var displayMessages = function (person, messages) {
+  //adds the click listener to the chat heads once they have been loaded
+  var addChatHeadClicker = function () {
+    $('.chat-head').on('click', function () {
+      var person = Number($(this).attr('data'));
+      messageFocus = person;
+      displayMessages(person, messages);
+    });
+  };
 
+  //when you click on a chat head it displays the messages you have with that person
+  var displayMessages = function (messageFocus, messages) {
+    console.log('this far');
     $('#messages-display').html('');
-
     _.each(messages, function (message) {
       //gets the id of the user who sent/received the message
       var messageFrom = _.find(users, function (user){
@@ -77,7 +93,7 @@ $(document).ready(function () {
       });
 
       //if the message is either to or from the chat head you clicked it will display
-      if (messageFrom.id === person || messageTo.id === person) {
+      if (messageFrom.id === messageFocus || messageTo.id === messageFocus) {
         var $newMessage = $('<div/>');
         var $messageHeader = $('<p/>');
         if (message.user_id === current_user) {
@@ -93,19 +109,21 @@ $(document).ready(function () {
         $('#messages-display').append($newMessage);
       };
     });
+    createMessageInput();
+  };
+
+  var createMessageInput = function () {
     var $messageInput = $('<textarea/>').attr({'id': 'message-content', 'placeholder': 'Enter new message here'});
-    $messageInput.appendTo('#messages-display');
-    var $submitMessage = $('<button/>').attr({'id': 'create-message', 'value': person}).text('Send message');
-    $submitMessage.appendTo('#messages-display');
+    $messageInput.appendTo('#create-message-box');
+    var $submitMessage = $('<button/>').attr({'id': 'create-message', 'value': messageFocus}).text('Send message');
+    $submitMessage.appendTo('#create-message-box');
 
     $($submitMessage).on('click', function () {
-      sendMessage(person);
+      sendMessage(messageFocus);
     });
   };
 
-  //calling getUserId to kick off the chain of functions
-  getUserId();
-
+  //when you click send message it will send a message (duh)
   var sendMessage = function (recipient) {
     var errors = 0;
     var messageGetter = recipient;
@@ -115,7 +133,7 @@ $(document).ready(function () {
     if (message === '') {
       errors += 1;
       var $messageError = $('<p/>').text('Please enter some message text');
-      $('#messages-display').append($messageError);
+      $('#create-message-box').append($messageError);
       $('#message-content').focus();
       setTimeout(function () {
         $messageError.remove();
@@ -124,21 +142,29 @@ $(document).ready(function () {
 
     //if there is not an error it will send the message to the recipient
     if (errors === 0) {
-        $.ajax({
-          method: 'POST',
-          url: '/messages',
-          data: {
-            message: {
-              user_id: current_user,
-              target: messageGetter,
-              message: message,
-              seen: false,
-              archive: false
-            }
+      $.ajax({
+        method: 'POST',
+        url: '/messages',
+        data: {
+          message: {
+            user_id: current_user,
+            target: messageGetter,
+            message: message,
+            seen: false,
+            archive: false
           }
-        });
+        }
+      }).done(function () {
         // reset message inputs
-        $('#message-content').val('');
-      };
+        $('#message-content').val('').focus();
+      });
+    };
   };
+
+
+
+  //calling getUserId to kick off the chain of functions
+  getUserId();
+
+
 });
